034ef9c0f3cc62ac860673964fa7b69b145acbbe
cyril grinko <k.a.grinko@gmail.com>
2024-10-17 16:30:33 +0300

    bloom_filter solved

--- 01-data-representation/tasks/bloom-filter/bloom_filter.c
+++ 01-data-representation/tasks/bloom-filter/bloom_filter.c
@@ -1,21 +1,46 @@
 #include "bloom_filter.h"
 
 uint64_t calc_hash(const char* str, uint64_t modulus, uint64_t seed) {
-    // TODO: Write any hash function for further usage
+  uint64_t hash = 0;
+  uint64_t m = 1;
+
+  for (int i = 0; str[i] != '\0'; ++i) {
+    int x = (int)(str[i] - ' ' + 1);
+    hash = (hash * seed + m * x) % modulus;
+  }
+
+  return hash;
 }
 
-void bloom_init(struct BloomFilter* bloom_filter, uint64_t set_size, hash_fn_t hash_fn, uint64_t hash_fn_count) {
-    // TODO: Initialize bloom filter
+void bloom_init(struct BloomFilter* bloom_filter, uint64_t set_size,
+                hash_fn_t hash_fn, uint64_t hash_fn_count) {
+  bloom_filter->set = calloc((set_size + 63) / 64, sizeof(uint64_t));
+  bloom_filter->set_size = set_size;
+  bloom_filter->hash_fn = hash_fn;
+  bloom_filter->hash_fn_count = hash_fn_count;
 }
 
 void bloom_destroy(struct BloomFilter* bloom_filter) {
-    // TODO: Free memory if needed
+  free(bloom_filter->set);
+  bloom_filter->set = NULL;
+  bloom_filter->set_size = 0;
+  bloom_filter->hash_fn = NULL;
+  bloom_filter->hash_fn_count = 0;
 }
 
 void bloom_insert(struct BloomFilter* bloom_filter, Key key) {
-    // TODO: Insert key into set
+  for (int i = 0; i < bloom_filter->hash_fn_count; ++i) {
+    uint64_t hash = bloom_filter->hash_fn(key, bloom_filter->set_size, i);
+    bloom_filter->set[hash / 64] |= (1ull << (hash % 64));
+  }
 }
 
 bool bloom_check(struct BloomFilter* bloom_filter, Key key) {
-    // TODO: Check if key exists in set
+  for (int i = 0; i < bloom_filter->hash_fn_count; ++i) {
+    uint64_t hash = bloom_filter->hash_fn(key, bloom_filter->set_size, i);
+    if (!(bloom_filter->set[hash / 64] & (1ull << (hash % 64)))) {
+      return false;
+    }
+  }
+  return true;
 }
--- 01-data-representation/tasks/bloom-filter/bloom_filter.h
+++ 01-data-representation/tasks/bloom-filter/bloom_filter.h
@@ -1,19 +1,21 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef uint64_t (*hash_fn_t)(const char*, uint64_t, uint64_t);
 
 uint64_t calc_hash(const char* str, uint64_t modulus, uint64_t seed);
 
 struct BloomFilter {
-    uint64_t* set;
-    uint64_t set_size;
-    hash_fn_t hash_fn;
-    uint64_t hash_fn_count;
+  uint64_t* set;
+  uint64_t set_size;
+  hash_fn_t hash_fn;
+  uint64_t hash_fn_count;
 };
 typedef const char* Key;
 
-void bloom_init(struct BloomFilter* bloom_filter, uint64_t set_size, hash_fn_t hash_fn, uint64_t hash_fn_count);
+void bloom_init(struct BloomFilter* bloom_filter, uint64_t set_size,
+                hash_fn_t hash_fn, uint64_t hash_fn_count);
 
 void bloom_destroy(struct BloomFilter* bloom_filter);
 
