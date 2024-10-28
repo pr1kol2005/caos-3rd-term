c58188c306db571a8f59169f6e2c5ce70098abc3
cyril grinko <k.a.grinko@gmail.com>
2024-10-28 20:43:22 +0300

    asm add scanf x86-64 done

--- 03-asm-1/tasks/asm-add-scanf/add_scanf.s
+++ 03-asm-1/tasks/asm-add-scanf/add_scanf.s
@@ -1,11 +1,25 @@
-  .intel_syntax noprefix
+.intel_syntax noprefix
 
-  .text
-  .global add_scanf
+.text
+.global add_scanf
 
 add_scanf:
-  # ???
+  push rbp
+  mov rbp, rsp
 
+  sub rsp, 16
+
+  lea rdi, [scanf_format_string]
+  lea rsi, [rsp]
+  lea rdx, [rsp + 8]
+
+  call scanf
+
+  mov rax, [rsp]
+  add rax, [rsp + 8]
+
+  mov rsp, rbp
+  pop rbp
   ret
 
   .section .rodata
