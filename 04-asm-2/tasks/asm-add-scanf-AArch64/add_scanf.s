66a7cd4f0c24ea850d9a20475a89665f3b283ab6
cyril grinko <k.a.grinko@gmail.com>
2024-10-28 19:29:01 +0300

    add scanf arm done

--- 04-asm-2/tasks/asm-add-scanf-AArch64/add_scanf.s
+++ 04-asm-2/tasks/asm-add-scanf-AArch64/add_scanf.s
@@ -12,11 +12,26 @@
   .endm
 
 add_scanf:
-  // ???
+  stp x29, x30, [sp, -16]!
+
+  sub sp, sp, 16
+  mov x1, sp
+  add x2, sp, 8
+  ldr x0, =scanf_format_string
+
+  bl scanf
+
+  ldr x0, [sp]
+  ldr x1, [sp, 8]
+  add x0, x0, x1
+
+  add sp, sp, 16
+  ldp x29, x30, [sp], 16
+  ret
 
   ret
 
-  .section .rodata
+.section .rodata, "a"
 
 scanf_format_string:
   .string "%lld %lld"
