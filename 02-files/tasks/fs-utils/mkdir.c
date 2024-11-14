689a11e28e677ad08432a22b89369a830cb9bc3f
cyril grinko <k.a.grinko@gmail.com>
2024-11-14 03:38:53 +0300

    fsutils done

--- 02-files/tasks/fs-utils/mkdir.c
+++ 02-files/tasks/fs-utils/mkdir.c
@@ -1,4 +1,70 @@
-int main() {
-    // TODO: implement me
-    return 0;
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+const mode_t RWE = 0777;
+const int PATH_MAX = 1024;
+
+void CreateParents(const char *path, mode_t mode) {
+  char temp[PATH_MAX];
+  strncpy(temp, path, sizeof(temp));
+  temp[sizeof(temp) - 1] = '\0';
+
+  char *parent = temp;
+  while ((parent = strchr(parent, '/')) != NULL) {
+    *parent = '\0';
+
+    if (strlen(temp) > 0) {
+      if (mkdir(temp, RWE) == -1 && errno != EEXIST) {
+        exit(1);
+      }
+    }
+    *parent = '/';
+    parent++;
+  }
+
+  if (mkdir(temp, mode) == -1 && errno != EEXIST) {
+    exit(1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int is_there_p = 0;
+  mode_t mode = RWE;
+  int current_option = 0;
+
+  struct option long_options[] = {{"mode", required_argument, 0, 'm'}};
+
+  while ((current_option = getopt_long(argc, argv, "pm:", long_options, NULL)) != -1) {
+    if (current_option == 'p') {
+      is_there_p = 1;
+    } else if (current_option == 'm') {
+      mode = strtol(optarg, NULL, 8);
+      if (mode == 0 && strcmp(optarg, "0") != 0) {
+        return 1;
+      }
+    } else {
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    return 1;
+  }
+
+  for (int i = optind; i < argc; i++) {
+    if (is_there_p) {
+      CreateParents(argv[i], mode);
+    } else {
+      if (mkdir(argv[i], mode) == -1) {
+        return 1;
+      }
+    }
+  }
+
+  return 0;
 }
--- 02-files/tasks/fs-utils/rm.c
+++ 02-files/tasks/fs-utils/rm.c
@@ -1,4 +1,85 @@
-int main() {
-    // TODO: implement me
-    return 0;
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+const int PATH_MAX = 1024;
+
+int RecursiveRemove(const char *path) {
+  struct dirent *entry;
+  DIR *dir = opendir(path);
+
+  if (!dir) {
+    return 1;
+  }
+
+  while ((entry = readdir(dir)) != NULL) {
+    char fullpath[PATH_MAX];
+    snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
+
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+      continue;
+
+    struct stat path_stat;
+    if (lstat(fullpath, &path_stat) == -1) {
+      return 1;
+    }
+
+    if (S_ISDIR(path_stat.st_mode)) {
+      if (RecursiveRemove(fullpath) == 1) {
+        return 1;
+      };
+      if (rmdir(fullpath) == -1) {
+        return 1;
+      }
+    } else {
+      if (remove(fullpath) == -1) {
+        return 1;
+      }
+    }
+  }
+  closedir(dir);
+  return 0;
+}
+
+int main(int argc, const char *argv[]) {
+  int recursive = 0;
+  int start = 1;
+
+  if (argc < 2) {
+    return 1;
+  }
+
+  if (strcmp(argv[1], "-r") == 0) {
+    recursive = 1;
+    start = 2;
+  }
+
+  for (int i = start; i < argc; i++) {
+    struct stat path_stat;
+    if (lstat(argv[i], &path_stat) == -1) {
+      return 1;
+    }
+
+    if (S_ISDIR(path_stat.st_mode)) {
+      if (recursive) {
+        if (RecursiveRemove(argv[i]) == 1) {
+          return 1;
+        };
+        if (rmdir(argv[i]) == -1) {
+          return 1;
+        }
+      } else {
+        return 1;
+      }
+    } else {
+      if (remove(argv[i]) == -1) {
+        return 1;
+      }
+    }
+  }
+
+  return 0;
 }
