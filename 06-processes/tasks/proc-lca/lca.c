adae3c3c46638e8219c48e9f7b0b46bd80c0e0ef
cyril grinko <k.a.grinko@gmail.com>
2024-12-03 21:41:35 +0300

    proc lca function implemented

--- 06-processes/tasks/proc-lca/lca.c
+++ 06-processes/tasks/proc-lca/lca.c
@@ -1,5 +1,61 @@
 #include "lca.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+const size_t MAX_PATH_LENGTH = 1024;
+const size_t MAX_LINE_LENGTH = 1024;
+
+pid_t get_parent_id(pid_t pid) {
+  pid_t parent_id = 0;
+
+  char proc_filename[MAX_PATH_LENGTH];
+  sprintf(proc_filename, "/proc/%d/status", pid);
+
+  FILE* process_information = fopen(proc_filename, "r");
+  if (process_information != NULL) {
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, MAX_LINE_LENGTH, process_information) != NULL) {
+      if (strncmp(line, "PPid", 4) == 0) {
+        parent_id = (pid_t)atoll(line + 8);
+        break;
+      }
+    }
+  }
+  fclose(process_information);
+
+  return parent_id;
+}
+
 pid_t find_lca(pid_t x, pid_t y) {
-    // TODO: find p = LCA(x, y)
+  pid_t x_parents[MAX_TREE_DEPTH];
+  pid_t y_parents[MAX_TREE_DEPTH];
+  pid_t* x_traversal = &x_parents[0];
+  pid_t* y_traversal = &y_parents[0];
+
+  while (x != 0) {
+    *x_traversal = x;
+    x = get_parent_id(x);
+    ++x_traversal;
+  }
+
+  while (y != 0) {
+    *y_traversal = y;
+    y = get_parent_id(y);
+    ++y_traversal;
+  }
+
+  --x_traversal;
+  --y_traversal;
+
+  while (x_traversal >= &x_parents[0] && y_traversal >= &y_parents[0] &&
+          *x_traversal == *y_traversal) {
+    --x_traversal;
+    --y_traversal;
+  }
+
+  ++x_traversal;
+
+  return *x_traversal;
 }
